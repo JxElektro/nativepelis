bfee4cc9be842a85e19d572533e681296b581d7d
Jhen Nunez <jxelektro@gmail.com>
2023-04-04 16:23:10 -0400

    Toggle button

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TouchableOpacity} from 'react-native';
+import { theme } from './src/config/theme';
+import test from './src/components/test';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [themeMode, setThemeMode] = useState('dark');
+
+  const toggleTheme = () => {
+    setThemeMode(themeMode === 'dark' ? 'light' : 'dark');
+  };
+
+  const currentStyles = styles(themeMode);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your ddapp!</Text>
+    <View style={currentStyles.container}>
       <StatusBar style="auto" />
+      <Text>Open up App.tsx to start working on your app! {themeMode}</Text>
+      <TouchableOpacity onPress={toggleTheme}>
+        <Text>Toggle Theme</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+
+const styles = (themeMode: string) => StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: themeMode === 'dark' ? theme.colors.dark.background : theme.colors.light.background,
+    
   },
 });
+// Path: src\config\theme.ts
---
+++ src/config/theme.js
@@ -0,0 +1,30 @@
+export const theme = {
+  fonts: {
+    regular: 'Roboto-Regular',
+    medium: 'Roboto-Medium',
+    bold: 'Roboto-Bold',
+    light: 'Roboto-Light',
+    thin: 'Roboto-Thin',
+  },
+  colors: {
+    light:
+    {
+      primary: '#00293c',
+      secondary: '#1E656D',
+      background: '#F1F3CE',
+      text: '#FFF',
+      textSecondary: '#000',
+      action: '#F62a00',
+    }
+    ,
+    dark: { // bust be opose to light
+      primary: '#00293c',
+      secondary: '#1E656D',
+      background: '#00293c',
+      text: '#FFF',
+      textSecondary: '#000',
+      action: '#F62a00',
+    }
+  }
+};
+
