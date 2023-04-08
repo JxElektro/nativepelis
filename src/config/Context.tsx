3a30f53e86cd02eabd6f8c3028dedefd9501aa9b
Jhen Nunez <jxelektro@gmail.com>
2023-04-08 13:58:20 -0400

    green theme

--- src/config/Context.tsx
+++ src/config/Context.tsx
@@ -1,27 +1,27 @@
 import React, { createContext, useState } from 'react';
 
 const lightTheme = {
-  primary: "#1083D6",
+  primary: "#4CAF50",
   title: "#2A3B47",
   content: "#697477",
   light: "#A0A7AC",
   border: "#EFF3F5",
   container: "#FFF",
-  body: "#FBFBFE",
+  body: "#E1F0E0",
   barStyle: "dark-content",
-  backgroundColor: "#FFF",
+  backgroundColor: "#F3F9F1",
 };
 
 const darkTheme = {
-  primary: "#52A5E0",
+  primary: "#4CAF50",
   title: "#EFF3F5",
   content: "#C8CDD0",
   light: "#A0A7AC",
   border: "#2A3B47",
   container: "#212E36",
-  body: "#192229",
+  body: "#203026",
   barStyle: "light-content",
-  backgroundColor: "#212E36",
+  backgroundColor: "#2C3E34",
 };
 
 type ContextType = {
